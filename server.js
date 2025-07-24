0be66cd6b3b357ee2980d02f75c65858c708e2ef
Izhar Khurshid <izhar7181@gmail.com>
2025-07-25 00:25:31 +0500

    server updated

--- server.js
+++ server.js
@@ -7,14 +7,10 @@ const favoritePlacesRoutes = require("./routes/favoritePlacesRoutes");
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 app.use('/api/favoriteplaces',favoritePlacesRoutes)
 
