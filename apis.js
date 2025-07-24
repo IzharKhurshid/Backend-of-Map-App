aebee1a1da34f61ca3c69a6d625d910dabe335d3
Izhar Khurshid <izhar7181@gmail.com>
2025-07-24 18:02:26 +0500

    changes made to gitignore

--- apis.js
+++
@@ -1,8 +0,0 @@
-const baseUrl = "http://localhost:8000/api/favoriteplaces"
-
-
-const postApi = `${baseUrl}/postFavoritePlace`
-const getApi = `${baseUrl}/getFavoritePlace`
-const updateApi = `${baseUrl}/updateFavoritePlace/:id`
-const deleteApi = `${baseUrl}/deleteFavoritePlace/:id`
-const getSpecificApi = `${baseUrl}/specificFavoritePlace/:id`
