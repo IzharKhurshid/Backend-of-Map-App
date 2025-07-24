f101ffe99060f259e7937c1dd54575eb49a12f6f
Izhar Khurshid <izhar7181@gmail.com>
2025-07-24 21:53:32 +0500

    server.js code updated

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(cors("*"));
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use('/api/favoriteplaces',favoritePlacesRoutes)
 
