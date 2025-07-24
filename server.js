92c773b826d09d49ff21e624d2b997675ac76963
Izhar Khurshid <izhar7181@gmail.com>
2025-07-24 21:12:09 +0500

    code updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
-const conectDB = require("./configs/database");
+const connectDB = require("./configs/database");
 const favoritePlacesRoutes = require("./routes/favoritePlacesRoutes");
 
 dotenv.config();
@@ -12,11 +12,11 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
-
 app.use('/api/favoriteplaces',favoritePlacesRoutes)
+
+connectDB();
 
 
 app.listen(PORT, () => {
   console.log(`Server is running on Port : ${PORT}`);
-  conectDB();
 });
