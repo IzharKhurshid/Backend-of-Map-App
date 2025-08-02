300be23f3c44758c4de720790d0e7c3ea160ffd7
Izhar Khurshid <izhar7181@gmail.com>
2025-08-02 14:56:55 +0500

    initial commit

---
+++ apis.js
@@ -0,0 +1,8 @@
+const baseUrl = "http://localhost:8000/api/favoriteplaces"
+
+
+const postApi = `${baseUrl}/postFavoritePlace`
+const getApi = `${baseUrl}/getFavoritePlace`
+const updateApi = `${baseUrl}/updateFavoritePlace/:id`
+const deleteApi = `${baseUrl}/deleteFavoritePlace/:id`
+const getSpecificApi = `${baseUrl}/specificFavoritePlace/:id`
